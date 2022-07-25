4e50acb94ee8d1baf21d327b77908a8ae9f802bd
ulyanahashchuk <ulyanahashchuk@gmail.com>
2022-07-25 12:47:27 +0200

    develop: add eslint, prettier, stylelint, tailwind

---
+++ components/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => <div>Home page</div>
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '868px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
+    },
+  },
+  plugins: [],
+}
